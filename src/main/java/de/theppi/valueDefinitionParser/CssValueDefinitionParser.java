b33100f6fbf5b9d4b1ee7762f1cbc072d7f20043
theppi <TuriS@github.com>
2019-05-23 22:26:17 +0200

    Added scraper for getting css definitions

--- src/main/java/de/theppi/valueDefinitionParser/CssValueDefinitionParser.java
+++ src/main/java/de/theppi/valueDefinitionParser/CssValueDefinitionParser.java
@@ -1,5 +1,13 @@
 package de.theppi.valueDefinitionParser;
 
-public class CssValueDefinitionParser {
+import java.util.HashMap;
 
+import de.theppi.valueDefinitionParser.scraper.HtmlScraper;
+
+public class CssValueDefinitionParser {
+    public static void main (String...args) {
+        HtmlScraper scraper = new HtmlScraper();
+        HashMap<String, CssValueDefinition> definitionMap = scraper.parse();
+        
+    }
 }
