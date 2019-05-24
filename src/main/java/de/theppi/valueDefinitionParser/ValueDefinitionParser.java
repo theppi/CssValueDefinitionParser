3ac61819326173a0d512bbbc1e2cd56b6fd313fe
theppi <TuriS@github.com>
2019-05-24 14:20:04 +0200

    Revised building process, renamed files and classes

--- src/main/java/de/theppi/valueDefinitionParser/ValueDefinitionParser.java
+++ src/main/java/de/theppi/valueDefinitionParser/ValueDefinitionParser.java
@@ -1,13 +1,13 @@
 package de.theppi.valueDefinitionParser;
 
 import java.util.HashMap;
-
+import de.theppi.valueDefinitionParser.parser.CssValueDefinitionParser;
 import de.theppi.valueDefinitionParser.scraper.HtmlScraper;
 
-public class CssValueDefinitionParser {
+public class ValueDefinitionParser {
     public static void main (String...args) {
         HtmlScraper scraper = new HtmlScraper();
         HashMap<String, CssValueDefinition> definitionMap = scraper.parse();
-        
+    	CssValueDefinitionParser parser = new CssValueDefinitionParser(System.in);
     }
 }
